fce7308c9075feefb56c5468389668094e668ebf
Perfect03 <zlobin_gg52926@mail.ru>
2026-03-11 10:07:42 +0500

    1.0.1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import {Lfstrm} from 'lifestream-api'
 
-const QINO = new Lfstrm({DOMAIN_API: 'tele2', DOMAIN_FRONT: 'qino.live'})
-export default QINO.azotExtension
+const GTV = new Lfstrm({DOMAIN_API: 'galam', DOMAIN_FRONT: 'galamtv.kz'})
+export default GTV.azotExtension
